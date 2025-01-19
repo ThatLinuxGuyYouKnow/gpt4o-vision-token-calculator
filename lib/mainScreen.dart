ee9322caa9bd657e53d98f751b2a8e88757e018b
Emmanuels-code <arocket04@gmail.com>
2025-01-19 17:31:27 +0100

    feature: updated UI

--- lib/mainScreen.dart
+++ lib/mainScreen.dart
@@ -4,8 +4,43 @@ class Mainscreen extends StatelessWidget {
   Mainscreen({super.key});
 
   Widget build(BuildContext context) {
+    bool isSmallScreenHeight = MediaQuery.of(context).size.height < 600;
+    bool isSmallScreenWidth = MediaQuery.of(context).size.width < 600;
     return Scaffold(
-      body: SingleChildScrollView(),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(Icons.seven_k),
+            SizedBox(
+              width: isSmallScreenWidth ? 20 : 40,
+            ),
+            Text('GP4o Vision Tokens')
+          ],
+        ),
+        backgroundColor: Colors.white,
+        bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(4.0),
+            child: Divider(
+              color: Colors.black,
+            )),
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            decoration: BoxDecoration(
+                color: Colors.black87, borderRadius: BorderRadius.circular(20)),
+            width: isSmallScreenHeight ? 200 : 400,
+            child: Center(
+              child: Icon(Icons.upload_file),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
