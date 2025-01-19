80474a2955110c19d298c4a5cff3c38547e9b181
Emmanuels-code <arocket04@gmail.com>
2025-01-19 16:47:21 +0100

    feature: single file UI component

---
+++ lib/mainScreen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Mainscreen extends StatelessWidget {
+  Mainscreen({super.key});
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(),
+    );
+  }
+}
